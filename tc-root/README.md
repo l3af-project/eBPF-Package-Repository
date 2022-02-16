9fc6ffbf277026f3d6ad2051a1ffee48432456da
RagalahariP <ragalahari.potti@walmart.com>
2022-02-16 16:41:52 +0000

    Add tc-root source

---
+++ tc-root/README.md
@@ -0,0 +1,18 @@
+# tc-root
+
+Root program for chaining multiple tc programs
+
+## How to build
+
+To build run:
+
+```
+make
+```
+
+See our [L3AF Development Environment](https://github.com/l3af-project/l3af-arch/blob/main/dev_environment/) for a quick and easy way to try out L3AF on your local machine.
+
+## Usage:
+```
+tc_root --cmd <start/stop> --iface <iface>
+```
---
+++ tc-root/tc_root_ingress_kern.c
@@ -0,0 +1,34 @@
+// Copyright Contributors to the L3AF Project.
+// SPDX-License-Identifier: GPL-2.0
+
+#include <uapi/linux/bpf.h>
+#include "bpf_helpers.h"
+#include "tc_root_common.h"
+
+struct bpf_elf_map SEC("maps") tc_ingress_root_pass_array = {
+  .type = BPF_MAP_TYPE_PROG_ARRAY,
+  .size_key = sizeof(int),
+  .size_value = sizeof(int),
+  .pinning = PIN_GLOBAL_NS,
+  .max_elem = ROOT_ARRAY_SIZE,
+};
+
+struct bpf_elf_map SEC("maps") tc_ingress_root_array = {
+  .type = BPF_MAP_TYPE_PROG_ARRAY,
+  .size_key = sizeof(int),
+  .size_value = sizeof(int),
+  .pinning = PIN_GLOBAL_NS,
+  .max_elem = ROOT_ARRAY_SIZE,
+};
+
+SEC("tc-ingress-root")
+int tc_ingress_root(struct __sk_buff *skb) {
+  bpf_tail_call(skb, &tc_ingress_root_pass_array, 0);
+  return TC_ACT_OK;
+}
+
+SEC("tc-ingress-pass-through")
+int tc_ingress_pass_through(struct __sk_buff *skb) {
+  bpf_tail_call(skb, &tc_ingress_root_array, 0);
+  return TC_ACT_OK;
+}
