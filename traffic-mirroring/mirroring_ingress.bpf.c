9ac5277d34f57a2092603e899194c5136a8aab9d
Jay Sheth <jaykumar.sheth@walmart.com>
2025-03-18 06:29:35 +0000

    Adding the required header file
    Signed-off-by: Jay Sheth <jaykumar.sheth@walmart.com>

--- traffic-mirroring/mirroring_ingress.bpf.c
+++ traffic-mirroring/mirroring_ingress.bpf.c
@@ -10,6 +10,7 @@
 #include "vmlinux.h"
 #include "bpf_endian.h"
 #include "bpf_helpers.h"
+#include "bpf/bpf.h"
 #define ETH_P_IP	0x0800		/* Internet Protocol packet	*/
 #define ETH_P_ARP	0x0806		/* Address Resolution packet	*/
 #define ETH_HLEN	14		/* Total octets in header.	 */
@@ -26,7 +27,7 @@ struct saddr_key {
 };
 
 #define MAX_ADDRESSES 50
-#define KEY_SIZE_IPV4 sizeof(__u32) + sizeof(__u8) + sizeof(__u32)
+#define KEY_SIZE_IPV4 sizeof(struct bpf_lpm_trie_key) + sizeof(__u32)
 
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
